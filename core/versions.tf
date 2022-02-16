c048de1dccb73d4a715cccd6f0a4ef8fbda5e102
Arthur Chiao <16827192+acchiao@users.noreply.github.com>
2022-02-16 17:08:11 -0600

    Refactor provider list

--- core/versions.tf
+++ core/versions.tf
@@ -1,32 +1,34 @@
 terraform {
   required_version = "~> 1.1.5"
 
+  # Providers
+  # https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/random/latest/docs
+  # https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
+  # https://registry.terraform.io/providers/okta/okta/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/tls/latest/docs
+
   required_providers {
-    # See https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.17.1"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/random/latest/docs
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
 
-    # See https://registry.terraform.io/providers/cloudflare/cloudflare/latest/docs
     cloudflare = {
       source  = "cloudflare/cloudflare"
       version = "~> 3.9.1"
     }
 
-    # See https://registry.terraform.io/providers/okta/okta/latest/docs
     okta = {
       source  = "okta/okta"
       version = "~> 3.21.0"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/tls/latest/docs
     tls = {
       source  = "hashicorp/tls"
       version = "~> 3.1.0"
@@ -34,8 +36,8 @@ terraform {
   }
 
   cloud {
-    organization = "acchiao"
     hostname     = "app.terraform.io"
+    organization = "acchiao"
 
     workspaces {
       tags = ["raccoon", "core"]
--- stack/versions.tf
+++ stack/versions.tf
@@ -1,32 +1,34 @@
 terraform {
   required_version = "~> 1.1.5"
 
+  # Providers
+  # https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/random/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/helm/latest/docs
+  # https://registry.terraform.io/providers/hashicorp/tfe/latest/docs
+
   required_providers {
-    # See https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.17.1"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/random/latest/docs
     random = {
       source  = "hashicorp/random"
       version = "~> 3.1.0"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = "~> 2.7.1"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/helm/latest/docs
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.4.1"
     }
 
-    # See https://registry.terraform.io/providers/hashicorp/tfe/latest/docs
     tfe = {
       source  = "hashicorp/tfe"
       version = "~> 0.28.1"
