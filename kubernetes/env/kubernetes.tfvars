6fa8ee05c2f81d19b1587bce4eac96a9bb5a5867
Arthur Chiao <16827192+acchiao@users.noreply.github.com>
2022-03-08 18:09:10 -0600

    kubernetes: Remove unused variables

--- kubernetes/env/kubernetes.tfvars
+++ kubernetes/env/kubernetes.tfvars
@@ -1,11 +1,6 @@
-auto_scale          = true
 cloudflare_domain   = "raccoon.team"
-cluster_size        = "s-2vcpu-2gb"
 digitalocean_domain = "acchiao.com"
 environment         = "production"
-max_nodes           = 5
-min_nodes           = 1
-node_count          = 1
 project_name        = "raccoon"
 region              = "nyc1"
 release             = "stable"
--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -13,46 +13,10 @@ variable "project_name" {
   type        = string
 }
 
-variable "project_environment" {
-  description = "The DigitalOcean project environment. The possible values are: `Development`, `Staging`, `Production`."
-  type        = string
-  default     = "Production"
-}
-
 variable "region" {
   description = "See `doctl kubernetes options regions`."
   type        = string
   default     = "nyc1"
-}
-
-variable "cluster_size" {
-  description = "See `doctl kubernetes options sizes`."
-  type        = string
-  default     = "s-1vcpu-2gb"
-}
-
-variable "node_count" {
-  description = "The number of Droplet instances in the node pool."
-  type        = number
-  default     = 1
-}
-
-variable "auto_scale" {
-  description = "Enable auto-scaling for the Kubernetes cluster within given min/max node range."
-  type        = bool
-  default     = true
-}
-
-variable "min_nodes" {
-  description = "If auto-scaling is enabled, this represents the minimum number of nodes that the node pool can be scaled down to."
-  type        = number
-  default     = 1
-}
-
-variable "max_nodes" {
-  description = "If auto-scaling is enabled, this represents the maximum number of nodes that the node pool can be scaled up to."
-  type        = number
-  default     = 1
 }
 
 variable "digitalocean_domain" {
@@ -65,12 +29,6 @@ variable "cloudflare_domain" {
   description = "The name of the Cloudflare domain resource."
   type        = string
   default     = ""
-}
-
-variable "install_charts" {
-  description = "If true, Helm chart releases will be installed."
-  type        = bool
-  default     = false
 }
 
 variable "helm_wait" {
