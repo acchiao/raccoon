e47457a9c7aee14586d14a37c067858647754a17
Arthur Chiao <16827192+acchiao@users.noreply.github.com>
2022-05-19 19:26:54 -0500

    Add kubed chart

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -162,6 +162,33 @@ resource "helm_release" "datadog" {
   ]
 }
 
+resource "kubernetes_namespace" "kubed" {
+  metadata {
+    name = "kubed"
+  }
+}
+
+resource "helm_release" "kubed" {
+  name       = "kubed"
+  repository = "https://charts.appscode.com/stable"
+  chart      = "kubed"
+  namespace  = kubernetes_namespace.kubed.metadata[0].name
+  version    = var.kubed_version
+
+  lint    = true
+  wait    = var.helm_wait
+  timeout = var.helm_timeout
+
+  set {
+    name  = "enableAnalytics"
+    value = "false"
+  }
+
+  depends_on = [
+    kubernetes_namespace.kubed,
+  ]
+}
+
 # resource "kubernetes_namespace" "metrics" {
 #   metadata {
 #     name = "metrics"
